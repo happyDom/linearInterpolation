78dc50edb9c03342289ac97c87487fa91121a62b
dyyP50Workstation <douyaoyuan@126.com>
2024-11-06 19:23:39 +0800

    feat: 完成了二维线性查表的功能

--- src/linearInterpolation.c
+++ src/linearInterpolation.c
@@ -15,17 +15,14 @@ void tableInitXYZ(linearInterpolation_t *pTable, uint8_t sizeOfX, uint8_t sizeOf
   if(NULL == pTable){return;}
   if(sizeOfX < 1){sizeOfX=1;}
   if(sizeOfY < 1){sizeOfY=1;}
-  if(sizeOfY < sizeOfX){sizeOfY = sizeOfX;}
 
   pTable->sizeOfX = sizeOfX;
   pTable->sizeOfY = sizeOfY;
 
   pTable->pxValues = (float *)malloc(sizeof(float) * pTable->sizeOfX);
   pTable->pyValues = (float *)malloc(sizeof(float) * pTable->sizeOfY);
-  pTable->pzValues = (float **)malloc(sizeof(float) * pTable->sizeOfY);
-  for(uint8_t i = 0;i < pTable->sizeOfY; i++){
-    pTable->pzValues[i] = (float *)malloc(sizeof(float) * pTable->sizeOfX);
-  }
+  pTable->pzValuesOfY = (float **)malloc(sizeof(float) * pTable->sizeOfY);
+  for(uint8_t i = 0;i < pTable->sizeOfY; i++){pTable->pzValuesOfY[i] = (float *)malloc(sizeof(float) * pTable->sizeOfX);}
 }
 
 float getY(linearInterpolation_t *pTable, float x){//一维查表，由(x)坐标查y值
@@ -50,5 +47,47 @@ float getY(linearInterpolation_t *pTable, float x){//一维查表，由(x)坐标
 }
 
 float getZ(linearInterpolation_t *pTable, float x, float y){//二维查表，由(x,y)坐标，查z值
-  return 0; //todo
+  linearInterpolation_t zTableY0;
+  zTableY0.sizeOfX = pTable->sizeOfX;
+  zTableY0.pxValues = pTable->pxValues;
+
+  if(y <= pTable->pyValues[0]){
+    zTableY0.pyValues = pTable->pzValuesOfY[0];
+    return getY(&zTableY0, x);
+  }
+  if(y >= pTable->pyValues[pTable->sizeOfY-1]){
+    zTableY0.pyValues = pTable->pzValuesOfY[pTable->sizeOfY-1];
+    return getY(&zTableY0, x);
+  }
+
+  //根据给定的y值，找到y所在位置的上界和下界
+  for(unsigned int i=0; i<pTable->sizeOfY-1; i++){
+    if(y == pTable->pyValues[i+1]){
+      zTableY0.pyValues = pTable->pzValuesOfY[i+1];
+      return getY(&zTableY0, x);
+    }
+
+    if(y < pTable->pyValues[i+1]){
+      if(y == pTable->pyValues[i]){
+        zTableY0.pyValues = pTable->pzValuesOfY[i];
+        return getY(&zTableY0, x);
+      }
+
+      linearInterpolation_t zTableY1;
+      float z0,z1;
+      
+      zTableY1.sizeOfX = pTable->sizeOfX;
+      zTableY1.pxValues = pTable->pxValues;
+
+      zTableY0.pyValues = pTable->pzValuesOfY[i];
+      zTableY1.pyValues = pTable->pzValuesOfY[i+1];
+      z0 = getY(&zTableY0, x);
+      z1 = getY(&zTableY1, x);
+
+      return z0 + (z1 - z0) * (y - pTable->pyValues[i]) / (pTable->pyValues[i+1] - pTable->pyValues[i]);
+    }
+  }
+
+  //default
+  return 0;
 }
--- src/main.c
+++ src/main.c
@@ -4,22 +4,54 @@
 linearInterpolation_t demoTable;
 
 int main(int argc, char const *argv[])
-{   
-    tableInitXY(&demoTable, 3);
-    demoTable.pxValues[0] = 0;
-    demoTable.pxValues[1] = 2;
-    demoTable.pxValues[2] = 4;
-    demoTable.pyValues[0] = 10;
-    demoTable.pyValues[1] = 15;
-    demoTable.pyValues[2] = 20;
+{
+    if(false){
+        //使用 init 方法配置表参数
+        tableInitXY(&demoTable, 3);
+        demoTable.pxValues[0] = 0;
+        demoTable.pxValues[1] = 2;
+        demoTable.pxValues[2] = 4;
 
-    float userNumber=0;
-    while (true)
-    {
-        printf("please input a number:"); scanf("%f", &userNumber);
-        printf("the x is: %f, then the y should be: %f\n", userNumber, getY(&demoTable, userNumber));
+        free(demoTable.pyValues);
+        static float yValues[] = {10.0f, 15.0f, 20.0f};
+        demoTable.pyValues = yValues;
+
+        float userX=0;
+        while (true)
+        {
+            printf("please input X value:"); scanf("%f", &userX);
+            printf("the x is: %f, then the y should be: %f\n", userX, getY(&demoTable, userX));
+        }
+    }
+
+    if(true){
+        //自定义表参数
+        demoTable.sizeOfX = 2;
+        demoTable.sizeOfY = 3;
+
+        //static的约束保障了这些变量的空间不会被收回
+        static float xValues[] = {1,2};
+        static float yValues[] = {1,2,3};
+
+        demoTable.pxValues = xValues;
+        demoTable.pyValues = yValues;
+        demoTable.pzValuesOfY = (float **)malloc(sizeof(float) * demoTable.sizeOfY);
+
+        static float pzValuesOfY_0[] = {2,3};
+        static float pzValuesOfY_1[] = {3,4};
+        static float pzValuesOfY_2[] = {4,5};
+        demoTable.pzValuesOfY[0] = pzValuesOfY_0;   //把每一个y对应一行z值配置到表中
+        demoTable.pzValuesOfY[1] = pzValuesOfY_1;
+        demoTable.pzValuesOfY[2] = pzValuesOfY_2;
+
+        float userX=0, userY=0;
+        while (true)
+        {
+            printf("please input X value:"); scanf("%f", &userX);
+            printf("please input Y value:"); scanf("%f", &userY);
+            printf("the coord is: (%f, %f), then the z should be: %f\n", userX, userY, getZ(&demoTable, userX, userY));
+        }
     }
     
-
     return 0;
 }
